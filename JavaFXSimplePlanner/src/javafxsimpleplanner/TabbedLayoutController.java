e4037e0d67fbef566b0bf41514355f689c7e3c2b
hadinajfi77 <hadinajafi.77@gmail.com>
2018-12-16 23:22:29 +0330

    tabblayout controller for FXML

---
+++ JavaFXSimplePlanner/src/javafxsimpleplanner/TabbedLayoutController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javafxsimpleplanner;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.ToolBar;
+
+/**
+ * FXML Controller class
+ *
+ * @author hadin
+ */
+public class TabbedLayoutController implements Initializable {
+
+    @FXML
+    private TabPane tabPane;
+
+    @FXML
+    private ToolBar toolbar;
+    
+    
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
