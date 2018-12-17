daf8ae10fb7f52a17e6dd315082f15ab59db2f83
hadinajfi77 <hadinajafi.77@gmail.com>
2018-12-17 22:25:54 +0330

    added datePicker and initialize the datePicker and Label for showing detailed time, by event handler.

--- JavaFXSimplePlanner/src/javafxsimpleplanner/TabbedLayoutController.java
+++ JavaFXSimplePlanner/src/javafxsimpleplanner/TabbedLayoutController.java
@@ -6,10 +6,20 @@
 package javafxsimpleplanner;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.ToolBar;
 
 /**
@@ -25,13 +35,42 @@ public class TabbedLayoutController implements Initializable {
     @FXML
     private ToolBar toolbar;
     
+    @FXML
+    private TableView<?> dayTableView;
+
+    @FXML
+    private TableColumn<?, ?> dayTimeColomun;
+
+    @FXML
+    private TableColumn<?, ?> dayTaskColumn;
+
+    @FXML
+    private TableColumn<?, ?> dayStateColumn;
+    
+    @FXML
+    private DatePicker datePicker;
+    
+    @FXML
+    private TextArea dayTextArea;
+    
+    @FXML
+    private Label dayTodayLabel;
+    
     
     /**
      * Initializes the controller class.
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        LocalDate date = LocalDate.now();
+        datePicker.setValue(date);
+        dayTodayLabel.setText(datePicker.getValue().format(DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy")));
+        
+        datePicker.addEventHandler(EventType.ROOT, (Event event) -> {
+            dayTodayLabel.setText(datePicker.getValue().format(DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy")));
+        });
     }    
     
 }
