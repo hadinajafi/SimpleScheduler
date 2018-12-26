d7c80b561e8c66bae88f02bb5a64e568f18e9063
hadinajfi77 <hadinajafi.77@gmail.com>
2018-12-26 22:32:59 +0330

    add priority combo box to the add new task window. also star image for combo box added with implementing cell factory of combo box. there is 3 type of priority, low, medium, high. low uses green star, medium uses yellow star, and high uses red.

--- JavaFXSimplePlanner/src/javafxsimpleplanner/AddNewTaskLayoutController.java
+++ JavaFXSimplePlanner/src/javafxsimpleplanner/AddNewTaskLayoutController.java
@@ -8,15 +8,23 @@ package javafxsimpleplanner;
 import java.net.URL;
 import java.time.LocalTime;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.util.Callback;
 
 /**
  * FXML Controller class
@@ -35,30 +43,30 @@ public class AddNewTaskLayoutController implements Initializable {
     private ProgressBar progressSlider;
 
     @FXML
-    private ChoiceBox<?> choiceBox;
-    
+    private ComboBox<String> priorityCombo;
+
     @FXML
     private Button increaseBtn;
 
     @FXML
     private Button decreaseBtn;
-    
+
     @FXML
     private Button applyBtn;
 
     @FXML
     private Button cancelBtn;
-    
+
     @FXML
     private TextArea detailsTextArea;
-    
+
     @FXML
     void decreaseProgressBar(MouseEvent event) {
         double value = progressSlider.getProgress();
         value -= 0.2;
         System.out.println(value);
         progressSlider.setProgress(value);
-        
+
         dynamicColorProgressBar(value);
     }
 
@@ -68,24 +76,20 @@ public class AddNewTaskLayoutController implements Initializable {
         value += 0.2;
         System.out.println(value);
         progressSlider.setProgress(value);
-        
+
         dynamicColorProgressBar(value);
     }
-    
-    private void dynamicColorProgressBar(double value){
-        if(value >= 0.19 && value < 0.4){
+
+    private void dynamicColorProgressBar(double value) {
+        if (value >= 0.19 && value < 0.4) {
             progressSlider.setStyle("-fx-accent: red");
-        }
-        else if(value >= 0.39 && value < 0.6){
-           progressSlider.setStyle("-fx-accent: orange;");
-        }
-        else if (value >= 0.59 && value < 0.8){
+        } else if (value >= 0.39 && value < 0.6) {
+            progressSlider.setStyle("-fx-accent: orange;");
+        } else if (value >= 0.59 && value < 0.8) {
             progressSlider.setStyle("-fx-accent: yellow");
-        }
-        else if (value >= 0.79 && value < 0.95){
+        } else if (value >= 0.79 && value < 0.95) {
             progressSlider.setStyle("-fx-accent: lightgreen");
-        }
-        else if(value >= 0.95){
+        } else if (value >= 0.95) {
             progressSlider.setStyle("-fx-accent: green");
         }
     }
@@ -104,20 +108,75 @@ public class AddNewTaskLayoutController implements Initializable {
         toSpinner.setMaxWidth(gridView.getPrefWidth());
         gridView.add(fromSpinner, 1, 1);
         gridView.add(toSpinner, 1, 2);
-        
-            double value = progressSlider.getProgress();
-            if (value < 25) {
-                progressSlider.setProgress(0);
-            } else if (25 <= value && value < 50) {
-                progressSlider.setProgress(25);
-            } else if (50 <= value && value < 75) {
-                progressSlider.setProgress(50);
-            } else if (75 <= value && value < 100) {
-                progressSlider.setProgress(75);
-            } else if (value == 100) {
-                progressSlider.setProgress(100);
+
+        String[] str = {"Low Priority", "Medium Priority", "High Priority"};
+        priorityCombo.getItems().addAll(str);
+        ListCell<String> cell = new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    ImageView imageView;
+                    if ("Medium Priority".equals(item)) {
+                        imageView = new ImageView(new Image("/icons/yellow-star16.png"));
+                    } else if (item.equals("Low Priority")) {
+                        imageView = new ImageView(new Image("/icons/green-star16.png"));
+                    } else {
+                        imageView = new ImageView(new Image("/icons/red-star16.png"));
+                    }
+                    setGraphic(imageView);
+                    setText(item);
+                }
             }
-        
+
+        };
+        Callback<ListView<String>, ListCell<String>> call = (ListView<String> param) -> new ListCell<String>() {
+            @Override
+            protected void updateItem(String item, boolean empty) {
+                super.updateItem(item, empty);
+                if (empty) {
+                    setGraphic(null);
+                } else {
+                    ImageView imageView;
+                    if ("Medium Priority".equals(item)) {
+                        imageView = new ImageView(new Image("/icons/yellow-star16.png"));
+                    } else if (item.equals("Low Priority")) {
+                        imageView = new ImageView(new Image("/icons/green-star16.png"));
+                    } else {
+                        imageView = new ImageView(new Image("/icons/red-star16.png"));
+                    }
+                    setGraphic(imageView);
+                    setText(item);
+                }
+            }
+        };
+        priorityCombo.setButtonCell(cell);
+        priorityCombo.setCellFactory(call);
+
+//        priorityCombo.setCellFactory((ListView<String> param) -> new ListCell<String>(){
+//            @Override
+//            protected void updateItem(String item, boolean empty) {
+//                super.updateItem(item, empty); //To change body of generated methods, choose Tools | Templates.
+//                if(empty){
+//                    setGraphic(null);
+//                }
+//                else{
+//                    ImageView imageView;
+//                    if("Medium".equals(item)){
+//                        imageView = new ImageView(new Image("/icons/yellow-star16.png"));
+//                    }
+//                    else if (item.equals("Low"))
+//                        imageView = new ImageView(new Image("/icons/green-star16.png"));
+//                    else{
+//                        imageView = new ImageView(new Image("/icons/red-star16.png"));
+//                    }
+//                    setGraphic(imageView);
+//                    setText(item);
+//                }
+//            }
+//        });
     }
 
 }
